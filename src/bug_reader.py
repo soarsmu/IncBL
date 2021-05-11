2ee3ed88c3c6657d167176b08a4907ab14d77be1
jiekeshi <jiekeshi125@gmail.com>
2021-05-11 19:33:32 +0800

    feat(bug_reader): implement bug_reader for local files

---
+++ src/bug_reader.py
@@ -0,0 +1,28 @@
+"""
+Read bug reports/issues and preprocess their text
+"""
+
+import xml.etree.ElementTree as ET
+from src.text_processor import text_processor
+
+def bug_reader_local(bug_reports_path: str):
+    """
+    Parse bug reports in ".xml" file format, such as Bugzbook dataset.
+    """
+
+    bug_data = {}
+    fixed_files = {}
+
+    tree = ET.parse(bug_reports_path)
+    root = tree.getroot()
+    for child in root:
+        bug_data[child.get("id")] = child[0].find("summary").text + child[0].find("description").text
+        fixed_files[child.get("id")] = child[1].find("file").text    
+    bug_data = text_processor(bug_data)
+    return bug_data, fixed_files
+
+# TODO: access github to get issues' content
+
+def bug_reader(bug_reports_path: str):
+
+    pass
