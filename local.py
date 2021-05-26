a7021c99b56f1a76f8715be1c9c0ef02ac5f6429
jiekeshi <jiekeshi125@gmail.com>
2021-05-26 19:34:13 +0800

    feat(): main feature ready to run

--- local.py
+++ local.py
@@ -1,13 +1,19 @@
 from src.incbl import incbl
+import argparse
 
 if __name__ == "__main__":
 
-    # TODO: argparse to add parameters
-    incbl = incbl(bug_reports_path, code_base_path)
-    incbl.index_update()
-    incbl.idf_update()
+    parser = argparse.ArgumentParser(description="A incremental bug localization tool")
+    parser.add_argument('bug_report_path', help='Bug report path')
+    parser.add_argument('code_base_path', help='Codebase directory')
+    parser.add_argument('-fe', '--file_extension', nargs='+', help='Suffixes of files to be processed', default=['java'])
+    args = parser.parse_args()
+
+    bug_report_path = args.bug_report_path
+    code_base_path = args.code_base_path
+    file_extension = args.file_extension
+    incbl = incbl(bug_report_path, code_base_path, file_extension)
     incbl.localization()
     incbl.evaluation()
-    incbl.fixed_bugs_update()
 
 
