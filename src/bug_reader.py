33cce75fdfd4535b8a7fd03d7fac265ec6b1a807
jiekeshi <jiekeshi125@gmail.com>
2021-05-26 19:33:48 +0800

    fix(bug reader): fixed files reader

--- src/bug_reader.py
+++ src/bug_reader.py
@@ -1,7 +1,7 @@
 """
 Read bug reports/issues and preprocess their text
 """
-
+import time
 import xml.etree.ElementTree as ET
 from src.text_processor import text_processor
 
@@ -17,12 +17,19 @@ def bug_reader_local(bug_reports_path: str):
     bug_data = {}
     fixed_files = {}
 
+    print("\n let's read the bug reports...\n")
+    
+    start_time = time.time()
+
     tree = ET.parse(bug_reports_path)
     root = tree.getroot()
     for child in root:
         bug_data[child.get("id")] = child[0].find("summary").text + child[0].find("description").text
-        fixed_files[child.get("id")] = child[1].find("file").text    
+        fixed_files[child.get("id")] = [file_path.text for file_path in child[1].findall("file")]
     bug_data = text_processor(bug_data)
+
+    print("the time overhead is ", time.time()-start_time)
+
     return bug_data, fixed_files
 
 # TODO: access github to get issues' content
