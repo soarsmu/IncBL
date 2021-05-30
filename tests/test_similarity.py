69b202c4de693097845f61e2dd21e0a837056330
jiekeshi <jiekeshi125@gmail.com>
2021-05-30 09:14:18 +0800

    feat(): test numpy data

--- tests/test_similarity.py
+++ tests/test_similarity.py
@@ -1,22 +1,28 @@
 import sys
 sys.path.append(".")
-from src.tfidf import tfidf_computing
+from src.tfidf import get_docu_feature,get_term_feature, tfidf_creation
 from src.similarity import compute_similarity
 from src.similarity import normalization
 from src.text_processor import text_processor
 
 def test_compute_similarity():
-    code_data = {"/zxing.appspot.com/generator/src/com/google/zxing/web/generator/client/StringConstants.java": "googl zxing web gener client googl zxing web gener client googl gwt client messag atempt local constant messag code type gener button", "/zxing.appspot.com/generator/src/com/google/zxing/web/generator/client/GeneratorSource.java": "googl zxing web gener client googl zxing web gener client googl gwt user client ui grid googl gwt user client ui widget gener sourc grid widget text gener valid widget widget gener set focu"}
+    code_data = {"/zxing.appspot.com/generator/src/com/google/zxing/web/generator/client/StringConstants.java": "googl zxing web gener client googl zxing web gener client googl gwt client messag atempt local constant messag code type gener button", "/zxing.appspot.com/generator/src/com/google/zxing/web/generator/client/GeneratorSource.java": "what the fucadahfa fafw fawf client googl g", "aa": "I loke igiey RNG"}
     code_data = text_processor(code_data)
 
-    bug_data = {"112": "googl ui widget gener sourc mothehuehge geighei client googl gwt client messag atempt local constant messag code type gener button"}
+    bug_data = {"112": "googl zxing web gener client googl zxing web gener client googl gwt client messag atempt local constant messag code type gener button"}
     bug_data = text_processor(bug_data)
 
-    expected_result = {}
+    word_list, dfs, idfs = get_docu_feature(code_data)
 
-    code_data, dct, model = tfidf_computing(code_data)
+    file_id, code_tfs = get_term_feature(word_list, code_data)
+
+    bug_id, bug_tfs = get_term_feature(word_list, bug_data)
+ 
+    bug_v = tfidf_creation(bug_id, bug_tfs, idfs)
     
-    print(compute_similarity(bug_data, code_data, dct, model))
+    code_v = tfidf_creation(file_id, code_tfs, idfs)
+    
+    print(compute_similarity(bug_v, code_v, bug_id, file_id))
 
 if __name__=="__main__":
 
--- tests/test_tfidf.py
+++ tests/test_tfidf.py
@@ -1,18 +1,19 @@
 import sys
 sys.path.append(".")
-from src.tfidf import tfidf_computing
+from src.tfidf import tfidf_creation
 from src.text_processor import text_processor
 
-def test_tfidf_computing():
+def test_tfidf_creation():
     code_data = {"/zxing.appspot.com/generator/src/com/google/zxing/web/generator/client/StringConstants.java": "googl zxing web gener client googl zxing web gener client googl gwt client messag atempt local constant messag code type gener button", "/zxing.appspot.com/generator/src/com/google/zxing/web/generator/client/GeneratorSource.java": "googl zxing web gener client googl zxing web gener client googl gwt user client ui grid googl gwt user client ui widget gener sourc grid widget text gener valid widget widget gener set focu"}
     code_data = text_processor(code_data)
 
     expected_result = {}
 
-    code_data, dct, model = tfidf_computing(code_data)
+    file_id, tf_idfs = tfidf_creation(code_data)
 
-    print(code_data.values())
+    print(tf_idfs)
+    
 
 if __name__=="__main__":
 
-    test_tfidf_computing()
+    test_tfidf_creation()
