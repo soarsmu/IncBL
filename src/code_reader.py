6ca7b72f90ba8e245a72c829ca42b18e23e7b3e0
jiekeshi <jiekeshi125@gmail.com>
2021-05-30 22:16:03 +0800

    feat(): multiprocessing and update code parser

--- src/code_reader.py
+++ src/code_reader.py
@@ -2,102 +2,79 @@
 Read local code files/github repos, commits and preprocess their text
 """
 import os
-from tree_sitter import Language, Parser
-from src.text_processor import text_processor
 import time
 
-def code_reader(code_base_path: str, extension: list) -> dict:
-    """
-    read code files and process code files
+from numpy.core.numeric import identity
+from tree_sitter import Language, Parser
+from multiprocessing import Queue, Pool, Manager
+from src.text_processor import text_processor
 
-    Args: string, local code store path
-
-    Returns: dict, {path: code content}
-    """
+def code_reader(code_file, file_type, q):
+    
     code_data = {}
-    code_length = {}
+    with open(code_file) as f:
+        if f.read(1):
+            code_data[code_file] = code_parser(f.read(), file_type)
+    code_data = text_processor(code_data)
+    
+    q.put(code_data)
 
+def mp_code_reader(code_base_path, file_type):
+
+    code_data = {}
+    
     print("\n let's read the code files...\n")
     start_time = time.time()
+    code_files = filter_files(code_base_path, file_type)
+
+    q = Manager().Queue()
+    pool = Pool(processes=8)
+    for code_file in code_files:
+        pool.apply_async(code_reader, args=(code_file[0], code_file[1], q))
+    pool.close()
+    pool.join()
+
+    for code_file in code_files:
+        code_data.update(q.get())
+
+    print("the overhead is ", time.time()-start_time)
+
+    return code_data
+
+def code_parser(code_cont, file_type):
+
+    if file_type == "py":
+        file_type = "python"
+    
+    parser = Parser()
+    parser.set_language(Language('./lib/languages.so', file_type))
+
+    parsed_code = str.encode(code_cont)
+    tree = parser.parse(parsed_code)
+    code_lines = code_cont.split('\n')
+
+    identifier = ""
+    nodes = [tree.root_node]
+    while nodes:
+        temp = []
+        for node in nodes:
+            for child in node.children:
+                temp.append(child)
+                if(child.type == "identifier"):
+                    identifier += code_lines[child.start_point[0]][child.start_point[1]:child.end_point[1]] + " "
+        nodes = temp
+
+    return code_cont + identifier
+
+
+def filter_files(code_base_path, file_type):
+
+    code_files = []
 
     dir_path = os.walk(code_base_path)
     for parent_dir, dir_name, file_names in dir_path:
         for file_name in file_names:
-            if(file_name.split(".")[-1].strip() in extension):
-                code_path = os.path.join(parent_dir, file_name)
-                with open(code_path) as f:
-                    if f.read(1):
-                        code_data[code_path] = code_parser(f.read())
-
-    code_data = text_processor(code_data)
-
-    for code_path, code_cont in code_data.items():
-        code_length[code_path] = len(code_cont)
-
-    print("the time overhead is ", time.time()-start_time)
-
-    return code_data, code_length
-
-def code_parser(code_cont: str):
-    """
-    parser code to get identifiers and methods
-
-    Args: string, raw code content
-
-    Returns: string
-    """
-
-    if not os.path.exists("./lib/languages.so"):
-        Language.build_library(
-            # Store the library in the `lib` directory
-            "./lib/languages.so",
-
-            # Attention! Change your own path to include one or more languages, need to clone the existing language repos from https://github.com/tree-sitter
-            [
-                "/home/jack/tree-sitter-java",
-            ]
-        )
-
-    # TODO: This part needs to be writen more clear.
-    JAVA_LANGUAGE = Language('./lib/languages.so', 'java')
-    parser_java = Parser()
-    parser_java.set_language(JAVA_LANGUAGE)
-    parsed_code = str.encode(code_cont)
-    tree = parser_java.parse(parsed_code)
+            if(file_name.split(".")[-1].strip() in file_type):
+                code_files.append([os.path.join(parent_dir, file_name), file_name.split(".")[-1].strip()])
     
-    cursor = tree.walk()
-    index = cursor.node.child_count
-    code_content_lines = code_cont.split('\n')
-    names = []
-    cursor.goto_first_child()
-    while not index == 0:
-        if(cursor.node.type == 'package_declaration'):
-            names.append(code_content_lines[cursor.node.start_point[0]][cursor.node.start_point[1]:cursor.node.end_point[1]])
-        if(cursor.node.type == 'class_declaration'):
-            cursor.goto_first_child()
-            while cursor.goto_next_sibling():
-                if(cursor.node.type == 'identifier'):
-                    names.append(code_content_lines[cursor.node.start_point[0]][cursor.node.start_point[1]:cursor.node.end_point[1]])
-            cursor.goto_first_child()
-            while cursor.goto_next_sibling():
-                if(cursor.node.type == 'method_declaration'):
-                    cursor.goto_first_child()
-                    while cursor.goto_next_sibling():
-                        if(cursor.node.type == 'identifier'):
-                            names.append(code_content_lines[cursor.node.start_point[0]][cursor.node.start_point[1]:cursor.node.end_point[1]])
-                    cursor.goto_parent()
-            cursor.goto_parent()
-            cursor.goto_parent()
-        cursor.goto_next_sibling()
-        index -= 1
-    return code_cont + str(names)
-
-def changed_code_reader():
-    """
-    for local evaluation, detect changed file and read it
-    
-    Args: string, local code store path
-
-    Returns: dict, {path: code content}
-    """
-    pass
+    return code_files
