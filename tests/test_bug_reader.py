5c3728bd461372fc01f7b6254bd2bbd01dd20ca7
jiekeshi <jiekeshi125@gmail.com>
2021-06-01 02:40:49 +0800

    feat(): test bug reader

--- tests/test_bug_reader.py
+++ tests/test_bug_reader.py
@@ -4,7 +4,8 @@ from src.bug_reader import bug_reader
 
 def test_bug_reader():
     
-    print(bug_reader("/home/jack/dataset/Bugzbook/tika/0.8.xml", "/home/jack/dataset/tika-1.20", ["java"]))
+    print(bug_reader("./data/tika.XML", "/home/jack/dataset/tika-1.20", ["java"]))
 
 if __name__ == "__main__":
+    
     test_bug_reader()
