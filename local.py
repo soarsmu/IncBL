c8c0f2f2613970ef87add4b055315f63865adf87
jiekeshi <jiekeshi125@gmail.com>
2021-06-01 02:43:34 +0800

    feat(): new data structure

--- local.py
+++ local.py
@@ -6,13 +6,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="A incremental bug localization tool")
     parser.add_argument('bug_report_path', help='Bug report path')
     parser.add_argument('code_base_path', help='Codebase directory')
-    parser.add_argument('-fe', '--file_extension', nargs='+', help='Suffixes of files to be processed', default=['java'])
+    parser.add_argument('-ft', '--file_type', nargs='+', help='Suffixes of files to be processed', default=['java'])
     args = parser.parse_args()
 
     bug_report_path = args.bug_report_path
     code_base_path = args.code_base_path
-    file_extension = args.file_extension
-    incbl = incbl(bug_report_path, code_base_path, file_extension)
+    file_type = args.file_type
+    incbl = incbl(bug_report_path, code_base_path, file_type)
     incbl.localization()
 
 
--- src/incbl.py
+++ src/incbl.py
@@ -1,76 +1,60 @@
 import os
 import time
 import numpy as np
-from src.bug_reader import bug_reader_local
-from src.code_reader import code_reader
+import warnings
+warnings.filterwarnings("ignore")
+from src.bug_reader import bug_reader
+from src.code_reader import mp_code_reader
 from src.tfidf import get_docu_feature, get_term_feature, tfidf_creation
-from src.similarity import compute_similarity, normalization, combine_bugs_simi
+from src.similarity import compute_similarity
 from src.evaluation import evaluation
 
 class incbl():
 
-    def __init__(self, bug_report_path, code_base_path, file_extension):
-        """
-        read bug reports and code
-        read index and model if have
-        read fixed_bugs
-        """
+    def __init__(self, bug_report_path, code_base_path, file_type):
+        
         self.bug_report_path = bug_report_path
         self.code_base_path = code_base_path
-        self.file_extension = file_extension
-        self.results = {}
+        self.file_type = file_type
 
     def localization(self):
-        """
-        query part
         
-        Args: index, model
-
-        Returns: dict, {ID: path}
-        """
-        print("\n localization starting...\n")
+        print("bug localization starting...")
+        
         start_time = time.time()
+        print("read bug reports...")
+        bug_data, fixed_files = bug_reader(self.bug_report_path, self.code_base_path, self.file_type)
+        print("the time consuming is %f s" %(time.time() - start_time))
 
-        bug_data, fixed_files = bug_reader_local(self.bug_report_path, self.code_base_path, self.file_extension)
-        code_data, code_length = code_reader(self.code_base_path, self.file_extension)
-        word_list, dfs, idfs = get_docu_feature(code_data)
+        start_time = time.time()
+        print("read code files...")
+        code_data = mp_code_reader(self.code_base_path, self.file_type)
+        print("the time consuming is %f s" %(time.time() - start_time))
 
-        file_id, code_tfs = get_term_feature(word_list, code_data)
+        start_time = time.time()
+        print("get the document-level features...")
+        idfs = get_docu_feature(code_data)
+        print("the time consuming is %f s" %(time.time() - start_time))
 
-        bug_id, bug_tfs = get_term_feature(word_list, bug_data)
-    
-        bug_v = tfidf_creation(bug_id, bug_tfs, idfs)
+        start_time = time.time()
+        print("get the term-level features...")
+        bug_vector = tfidf_creation(bug_data, idfs)
+        code_vector = tfidf_creation(code_data, idfs)
+        print("the time consuming is %f s" %(time.time() - start_time))
         
-        code_v = tfidf_creation(file_id, code_tfs, idfs)
-
-        similarity = compute_similarity(bug_v, code_v, bug_id, file_id)
-
-        similarity = normalization(similarity, code_length, bug_id, file_id)
-        
-        similarity = combine_bugs_simi(similarity, fixed_files, bug_data, bug_id, file_id)
+        start_time = time.time()
+        print("compute similarities...")
+        similarity = compute_similarity(bug_vector, code_vector)
+        print("the time consuming is %f s" %(time.time() - start_time))
+        similarity["score"] = -similarity["score"]
+        similarity = np.sort(similarity, order = "score")[:,:9]
+        similarity["score"] = -similarity["score"]
         print(similarity)
-        ranks = np.argsort(similarity, axis =0)
-        for i in ranks:
-            print(i)
         
-        #similarity = normalization(compute_similarity(bug_data, code_data, dct, model), code_length)
-        
-        #self.results = combine_bugs_simi(similarity, fixed_files, bug_data, self.code_base_path)
+        print("evaluation start...")
+        evaluation(fixed_files, similarity)
 
-        # print("the time overhead is ", time.time()-start_time, "\n The results is\n")
-
-        # for bug_id, code_files in self.results.items():
-        #     print(bug_id + ":" + "\n")
-        #     index = 0
-        #     print(fixed_files[bug_id])
-        #     for code_path, simi_score in code_files.items():
-        #         if not index >= 10:
-        #             print("\t" + code_path + "\t" + str(simi_score))
-        #             index += 1
-        #         else: break
-        
-        # evaluation(bug_data, fixed_files, self.code_base_path, self.results)
-
+       
     def tf_update(self):
         """
         update index matrices
