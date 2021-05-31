1f3d629f3ca5b2c85854005160563fc64e0b772d
jiekeshi <jiekeshi125@gmail.com>
2021-06-01 02:42:28 +0800

    fix(): evaluation for new data structure

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,14 +1,7 @@
 import os
 
-def evaluation(bug_data:dict, fixed_files:dict, code_base_path:str, results:dict):
-    """
-    computing the MAP, ACC, MRR values if have ground truth
+def evaluation(fixed_files, results):
 
-    Args: two dict, {ID: results}, {ID: fixed files} 
-
-    Returns: numbers, MAP, ACC, MRR values
-    """
-    print("\n evaluation starting...\n")
     acc = 0
     map_value = 0
 
@@ -17,19 +10,22 @@ def evaluation(bug_data:dict, fixed_files:dict, code_base_path:str, results:dict
     for bug_id, file_paths in fixed_files.items():
         temp = 0
         map_tmp = 0
-        if not file_paths.size() == 0:
+        if not len(file_paths) == 0:
             count += 1
             for file_path in file_paths:
                 truth_num += 1
-                for i, result in enumerate(results[bug_id].keys()):
-                    if result == file_path and i < 10:
-                        acc += 1
-                        temp += 1
-                        map_tmp += temp/(i+1.0)
+                for i in range(results.shape[0]):
+                    if results[i]["bug"][0] == bug_id.encode():
+                        for j in range(results.shape[1]):
+                            if results[i][j]["file"] == file_path.encode():
+                                acc += 1
+                                temp += 1
+                                map_tmp += temp/(j+1.0)
 
             if not temp == 0:
                 map_value += map_tmp / len(file_paths)
 
     acc /= truth_num
     map_value /= count
-    print("\t The accuracy @ top 10 is", acc, "\n", "\t The MAP @ top 10 is", map_value)
+    print("The accuracy @ top 10 is", acc)
+    print("The MAP @ top 10 is", map_value)
