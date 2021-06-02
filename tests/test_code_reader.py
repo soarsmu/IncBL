8d45c03138d799642c2fb19ac54c72bab80c441a
jiekeshi <jiekeshi125@gmail.com>
2021-06-02 14:49:09 +0800

    test database support

--- tests/test_code_reader.py
+++ tests/test_code_reader.py
@@ -3,8 +3,8 @@ sys.path.append(".")
 from src.code_reader import mp_code_reader, code_parser
 
 def test_code_reader():
-    codebase_path = "/home/jack/dataset/ZXing-1.6/bug/src/com/google/zxing/client/bug"
-    mp_code_reader(codebase_path, ["java", "py"])
+    codebase_path = "/home/jack/dataset/ZXing-1.6/bug/src/com/google/zxing/"
+    print(mp_code_reader(codebase_path, ["java"]))
 
 if __name__ == "__main__":
 
