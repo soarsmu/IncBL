88cc06cc1afcfd63a13e275d32fdeb1def4d4f0b
jiekeshi <jiekeshi125@gmail.com>
2021-06-03 14:08:58 +0800

    feat(): change to json storage

--- src/bug_reader.py
+++ src/bug_reader.py
@@ -1,28 +1,26 @@
 import os
+import json
 import xml.etree.ElementTree as ET
-from src.utils import db
 from src.text_processor import text_processor
 
-def bug_reader(bug_report_path, code_base_path, file_type):
+def bug_reader(bug_report_path, code_base_path, file_type, storage_path):
   
     bug_data = {}
-    fixed_files = {}
 
     tree = ET.parse(bug_report_path)
     root = tree.getroot()
     for child in root:
-        try:
-            bug_data[child.get("id")] = child[0].text + child[1].text
-        except: 
-            bug_data[child.get("id")] = child[0].text
-
-        fixed_files[child.get("id")] = []
+        if child[1].text:
+            bug_content = text_processor(child[0].text + child[1].text)
+        else:
+            bug_content = text_processor(child[0].text)
+        fixed_files = []
         for file_path in child[2].findall("file"):
             if file_path.text.split(".")[-1].strip() in file_type:
-                fixed_files[child.get("id")].append(os.path.join(code_base_path, file_path.text))
-    bug_data = text_processor(bug_data)
-
-    for bug_id, bug_cont in bug_data.items():
-        db.insert_one({"bug_id": bug_id, "bug_content": bug_cont, "fixed_files": fixed_files[bug_id]})
+                fixed_files.append(os.path.join(code_base_path, file_path.text))
+        bug_data[child.get("id")] = {"bug_content": bug_content, "fixed_files": fixed_files}
         
-    return bug_data, fixed_files
+    with open(os.path.join(storage_path + "bug_data.json"), "a") as f:
+        json.dump(bug_data, f, sort_keys=True)
+
+    return bug_data
