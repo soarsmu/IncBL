426d71055f103e5dfd887975adcd41e1f7bb41d4
jiekeshi <jiekeshi125@gmail.com>
2021-06-04 03:15:48 +0800

    fix(): fix map computing

---
+++ src/evaluation.py
@@ -0,0 +1,31 @@
+def evaluation(results, bug_data):
+    
+    acc = 0
+    map_value = 0
+
+    truth_num = 0
+    count = 0
+
+    for bug_id, bug_cont in bug_data.items():
+        temp = 0
+        map_tmp = 0
+        file_paths = bug_cont["fixed_files"]
+        if not len(file_paths) == 0:
+            count += 1
+            for file_path in file_paths:
+                truth_num += 1
+                for i in range(results.shape[0]):
+                    if results[i]["bug"][0] == bug_id.encode():
+                        for j in range(results.shape[1]):
+                            if results[i][j]["file"] == file_path.encode():
+                                acc += 1
+                                temp += 1
+                                map_tmp += temp/(j+1.0)
+
+            if not temp == 0:
+                map_value += map_tmp / len(file_paths)
+
+    acc /= truth_num
+    map_value /= count
+    print("The accuracy @ top 10 is", acc)
+    print("The MAP @ top 10 is", map_value)
