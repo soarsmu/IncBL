46a439173869c046f6c1d384ad057d026f44518a
jiekeshi <jiekeshi125@gmail.com>
2021-06-03 14:09:25 +0800

    feat(): support new json storage

--- src/text_processor.py
+++ src/text_processor.py
@@ -11,13 +11,13 @@ def extended_is_stop(token):
     stop_words = text.Defaults.stop_words
     return token.is_stop or token.lower_ in stop_words or token.lemma_ in stop_words
 
-def text_processor(docs: dict) -> dict:
+def text_processor(docs):
 
     # Text normalization: remove numbers and special symbols, and split identifiers
     pattern = re.compile(r"[a-zA-Z][a-z]+|[A-Z]+")
-    for id_, conts in docs.items():
-        # Stopword Removal and lemmatization
-        docs[id_] = [cont.lemma_.lower() for cont in text(str(pattern.findall(conts)))
-                                if not (extended_is_stop(cont) or cont.is_punct)]
+    
+    # Stopword Removal and lemmatization
+    docs = [word.lemma_.lower() for word in text(str(pattern.findall(docs))) 
+                            if not (extended_is_stop(word) or word.is_punct)]
                                     
     return docs
