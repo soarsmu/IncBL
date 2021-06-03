341ddf4146d202b835f195ae381019825c6afd2a
jiekeshi <jiekeshi125@gmail.com>
2021-06-03 21:53:19 +0800

    feat(): finish increntment

--- src/incbl.py
+++ src/incbl.py
@@ -5,17 +5,22 @@ import warnings
 warnings.filterwarnings("ignore")
 from src.bug_reader import bug_reader
 from src.code_reader import mp_code_reader
-from src.tfidf import get_docu_feature, get_term_feature, tfidf_creation
+from src.tfidf import get_docu_feature, tfidf_creation, update_tfidf_feature
 from src.similarity import compute_similarity
-from src.evaluation import evaluation
 
 class incbl():
 
-    def __init__(self, bug_report_path, code_base_path, file_type):
+    def __init__(self, bug_report_path, code_base_path, file_type, storage_path):
         
         self.bug_report_path = bug_report_path
         self.code_base_path = code_base_path
         self.file_type = file_type
+        self.code_storage_path = os.path.join(storage_path, "code/")
+        if not os.path.exists(self.code_storage_path):
+            os.mkdir(os.path.join(storage_path, "code/"))
+        self.bug_storage_path = os.path.join(storage_path, "bugs/")
+        if not os.path.exists(self.bug_storage_path):
+            os.mkdir(os.path.join(storage_path, "bugs/"))
 
     def localization(self):
         
@@ -23,25 +28,30 @@ class incbl():
         
         start_time = time.time()
         print("read bug reports...")
-        bug_data, fixed_files = bug_reader(self.bug_report_path, self.code_base_path, self.file_type)
+        bug_data = bug_reader(self.bug_report_path, self.code_base_path, self.file_type, self.bug_storage_path)
         print("the time consuming is %f s" %(time.time() - start_time))
 
         start_time = time.time()
         print("read code files...")
-        code_data = mp_code_reader(self.code_base_path, self.file_type)
+        code_data, added_files, deleted_files, modified_files = mp_code_reader(self.code_base_path, self.file_type, self.code_storage_path)
         print("the time consuming is %f s" %(time.time() - start_time))
 
-        start_time = time.time()
-        print("get the document-level features...")
-        idfs = get_docu_feature(code_data)
-        print("the time consuming is %f s" %(time.time() - start_time))
+        if not os.path.exists(os.path.join(self.code_storage_path, "idfs.npy")):
+            start_time = time.time()
+            print("get the document-level features...")
+            idfs = get_docu_feature(code_data, self.code_storage_path)
+            print("the time consuming is %f s" %(time.time() - start_time))
 
-        start_time = time.time()
-        print("get the term-level features...")
-        bug_vector = tfidf_creation(bug_data, idfs)
-        code_vector = tfidf_creation(code_data, idfs)
-        print("the time consuming is %f s" %(time.time() - start_time))
-        
+            start_time = time.time()
+            print("get the term-level features...")
+            bug_vector = tfidf_creation(bug_data, idfs, self.bug_storage_path)
+            code_vector = tfidf_creation(code_data, idfs, self.code_storage_path)
+            print("the time consuming is %f s" %(time.time() - start_time))
+        else: 
+            code_vector = update_tfidf_feature(code_data, added_files, deleted_files, modified_files, self.code_storage_path)
+            idfs = np.load(os.path.join(self.code_storage_path, "idfs.npy"))
+            bug_vector = tfidf_creation(bug_data, idfs, self.bug_storage_path)
+
         start_time = time.time()
         print("compute similarities...")
         similarity = compute_similarity(bug_vector, code_vector)
@@ -50,30 +60,34 @@ class incbl():
         similarity = np.sort(similarity, order = "score")[:,:9]
         similarity["score"] = -similarity["score"]
         print(similarity)
-        
-        print("evaluation start...")
-        evaluation(fixed_files, similarity)
-
        
-    def tf_update(self):
-        """
-        update index matrices
+    def evaluation(self):
 
-        Args: two dict, {path, code content}, {path, index}
-        """
-        pass
+        acc = 0
+        map_value = 0
 
-    def idf_update(self):
-        """
-        update index matrices
+        truth_num = 0
+        count = 0
+        for bug_id, file_paths in fixed_files.items():
+            temp = 0
+            map_tmp = 0
+            if not len(file_paths) == 0:
+                count += 1
+                for file_path in file_paths:
+                    truth_num += 1
+                    for i in range(results.shape[0]):
+                        if results[i]["bug"][0] == bug_id.encode():
+                            for j in range(results.shape[1]):
+                                if results[i][j]["file"] == file_path.encode():
+                                    acc += 1
+                                    temp += 1
+                                    map_tmp += temp/(j+1.0)
 
-        Args: two dict, {path, code content}, {path, index}
-        """
-        pass
+                if not temp == 0:
+                    map_value += map_tmp / len(file_paths)
 
-    def fixed_bugs_update(self):
-        """
-        save/update fixed bugs as .npy file
-        
-        """
-        pass
+        acc /= truth_num
+        map_value /= count
+        print("The accuracy @ top 10 is", acc)
+        print("The MAP @ top 10 is", map_value)
+
