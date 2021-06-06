dce42ff062d9c1e6bab2a2ed127a9e14ffdfaafb
jiekeshi <jiekeshi125@gmail.com>
2021-06-06 20:28:29 +0800

    fix(): fix bug reader for nonetype

--- src/bug_reader.py
+++ src/bug_reader.py
@@ -5,7 +5,7 @@ from dateutil.parser import parse
 from src.text_processor import text_processor
 
 def bug_reader(bug_report_path, code_base_path, file_type, storage_path):
-  
+
     bug_data = {}
 
     tree = ET.parse(bug_report_path)
@@ -17,8 +17,9 @@ def bug_reader(bug_report_path, code_base_path, file_type, storage_path):
             bug_content = text_processor(child[0].text)
         fixed_files = []
         for file_path in child[2].findall("file"):
-            if file_path.text.split(".")[-1].strip() in file_type:
-                fixed_files.append(os.path.join(code_base_path, file_path.text))
+            if file_path.text:
+                if file_path.text.split(".")[-1].strip() in file_type:
+                    fixed_files.append(os.path.join(code_base_path, file_path.text))
         open_date = parse(child.get("opendate"), ignoretz=True).isoformat()
         bug_data[child.get("id")] = {"content": bug_content, "fixed_files": fixed_files, "open_date": open_date}
 
