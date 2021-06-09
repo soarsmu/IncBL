d3ce64152b6f419d361843bbe2cb67e1ffb314db
Yang Zhou <zhou.yang.19@ucl.ac.uk>
2021-06-09 17:14:35 +0800

    change path

--- RQ2.sh
+++ RQ2.sh
@@ -1,9 +1,11 @@
 
-bug_reports_path="/home/jack/clean_bugzbook/remove_truth"
-results_path="/home/jack/results"
-codebase_path="/home/jack/codebase"
-incbl_root="/home/jack/Blinpy-app"
+bug_reports_path="/home/incbl_experiment_data/clean_bugzbook/remove_truth"
+results_path="/home/incbl_experiment_data/results"
+codebase_path="/home/incbl_experiment_data/codebases"
+incbl_root="/home/Blinpy-app"
 
+cd $bug_reports_path
+pwd
 
 for folder in `ls $1`
 do {
@@ -16,5 +18,5 @@ do {
         bug=$(echo $line|cut -d "l" -f 1)
         python "$incbl_root"/"local.py" "$incbl_root" "$bug_reports_path"/"$folder/$bug""l" "$codebase_path"/"$folder"
     done < $bug_reports_path"/"$folder"/""ver_time.txt"
-}  > $results_path"/"$folder".txt" &
+}  
 done
