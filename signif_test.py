806014569b7a6044f1d1a255336c956235a8bfa3
jiekeshi <jiekeshi125@gmail.com>
2021-06-15 15:55:24 +0800

    feat(): significance test

--- signif_test.py
+++ signif_test.py
@@ -1,17 +1,25 @@
 import numpy as np
 from scipy.stats import ttest_ind
-
-with open("./runtime.txt") as f:
+import pandas as pd
+with open("./runtime1.txt") as f:
     all_data = f.read().split("\n")
     with_ = []
     without_ = []
     for data in all_data:
-        if data.split(" ")[1] == "with":
+        if data.split(" ")[2] == "without":
             with_.append(float(data.split(" ")[-1]))
-        if data.split(" ")[1] == "without":
+        if data.split(" ")[1] == "with":
             without_.append(float(data.split(" ")[-1]))
 with_ = np.array(with_)
 without_ = np.array(without_)
 res = ttest_ind(with_, without_).pvalue
+print(res, len(with_), len(without_))
+rate = []
+for i in range(len(with_)):
+    rate.append(with_[i]/without_[i])
 
-print(res)
+# rate=pd.DataFrame(data=rate)
+
+# rate.to_csv('./rate.csv')
+
+# print(rate)
